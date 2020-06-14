9c4a6b985741be5ca982d2dd429fc72c919348d4
Roman Opalacz <roman9891@gmail.com>
2020-06-14 02:58:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,6 +6,28 @@ def game_hash
       colors: ["Black", "white"],
       players: [
         {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          player_name: "Reggie Evans",
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
           player_name:,
           number:,
           shoe:,
@@ -16,12 +38,90 @@ def game_hash
           blocks:,
           slam_dunks:
         },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        }
       ]
     },
     away: {
       team_name: ,
       colors: [],
-      players: []
+      players: [
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        }
+      ]
     }
   }
 end
