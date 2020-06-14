fa931e7e0ae7c178d8fdf90f790c613f446d0bcc
Roman Opalacz <roman9891@gmail.com>
2020-06-14 02:54:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,12 +2,24 @@
 def game_hash
   {
     home: {
-      team_name: {},
-      colors: [],
-      players: []
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "white"],
+      players: [
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+      ]
     },
     away: {
-      team_name: {},
+      team_name: ,
       colors: [],
       players: []
     }
