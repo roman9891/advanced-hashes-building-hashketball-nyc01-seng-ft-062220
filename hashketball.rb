55321c6a94fe44362cb729a252f6041dba96fd4c
Roman Opalacz <roman9891@gmail.com>
2020-06-14 03:07:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,45 +29,45 @@ def game_hash
         },
         {
           player_name: "Brook Lopez",
-          number:,
-          shoe:,
-          points:,
-          rebounds:,
-          assists:,
-          steals:,
-          blocks:,
-          slam_dunks:
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
         },
         {
-          player_name:,
-          number:,
-          shoe:,
-          points:,
-          rebounds:,
-          assists:,
-          steals:,
-          blocks:,
-          slam_dunks:
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
         },
         {
-          player_name:,
-          number:,
-          shoe:,
-          points:,
-          rebounds:,
-          assists:,
-          steals:,
-          blocks:,
-          slam_dunks:
+          player_name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
         }
       ]
     },
     away: {
-      team_name: ,
-      colors: [],
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
       players: [
         {
-          player_name:,
+          player_name: "Jeff Adrien",
           number:,
           shoe:,
           points:,
